6cfa561707ea483525b16e9729cced68874e57b7
boka <beni.r@panorays.com>
2022-05-05 09:31:08 +0300

    fix: moced to class extend

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const ItemStatusFilter = () => {
-  return (
-    <div className="btn-group">
-      <button type="button" className="btn btn-info">
-        All
-      </button>
-      <button type="button" className="btn btn-outline-secondary">
-        Active
-      </button>
-      <button type="button" className="btn btn-outline-secondary">
-        Done
-      </button>
-    </div>
-  );
-};
+export default class ItemStatusFilter extends Component {
+  render() {
+    // const {} = this.props;
+    return (
+      <div className="btn-group">
+        <button type="button" className="btn btn-info">
+          All
+        </button>
+        <button type="button" className="btn btn-outline-secondary">
+          Active
+        </button>
+        <button type="button" className="btn btn-outline-secondary">
+          Done
+        </button>
+      </div>
+    );
+  }
+}
 
-export default ItemStatusFilter;
+// const ItemStatusFilterFunc = () => {
+//   return (
+//     <div className="btn-group">
+//       <button type="button" className="btn btn-info">
+//         All
+//       </button>
+//       <button type="button" className="btn btn-outline-secondary">
+//         Active
+//       </button>
+//       <button type="button" className="btn btn-outline-secondary">
+//         Done
+//       </button>
+//     </div>
+//   );
+// };
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './todo-list-item.css';
 
-const TodoListItem = ({ label, important = false }) => {
-  const style = {
-    color: important ? 'tomato' : 'black',
-    fontWeight: important ? 'bold' : 'normal',
-  };
-  return (
-    <span className="todo-list-item" style={style}>
-      {label}
-      <button type="button" className="btn btn-outline-success">
-        <i className="fa fa-exclamation" />
-      </button>
+class TodoListItem extends Component {
+  render() {
+    const { label, important = false } = this.props;
 
-      <button type="button" className="btn btn-outline-danger">
-        <i className="fa trash-o" />
-      </button>
-    </span>
-  );
-};
+    const style = {
+      color: important ? 'tomato' : 'black',
+      fontWeight: important ? 'bold' : 'normal',
+    };
+    return (
+      <span className="todo-list-item" style={style}>
+        {label}
+        <button type="button" className="btn btn-outline-success">
+          <i className="fa fa-exclamation" />
+        </button>
+
+        <button type="button" className="btn btn-outline-danger">
+          <i className="fa fa-trash-o" aria-hidden="true"></i>
+        </button>
+      </span>
+    );
+  }
+}
 
 export default TodoListItem;
+
+// const TodoListItemFunc = ({ label, important = false }) => {
+//   const style = {
+//     color: important ? 'tomato' : 'black',
+//     fontWeight: important ? 'bold' : 'normal',
+//   };
+//   return (
+//     <span className="todo-list-item" style={style}>
+//       {label}
+//       <button type="button" className="btn btn-outline-success">
+//         <i className="fa fa-exclamation" />
+//       </button>
+//
+//       <button type="button" className="btn btn-outline-danger">
+//         <i className="fa fa-trash-o" aria-hidden="true"></i>
+//       </button>
+//     </span>
+//   );
+// };
