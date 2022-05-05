5df15a4327d0d66f91f4f1ccd3d38a23bc4a141b
boka <beni.r@panorays.com>
2022-05-05 10:03:37 +0300

    fix: added setState

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
+  state = { done: false };
+
   onLabelClick = () => {
-    console.log(`Done: ${this.props.label}`);
+    this.setState({ done: true });
   };
 
   render() {
     const { label, important = false } = this.props;
+    const { done } = this.state;
+
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done';
+    }
 
     const style = {
       color: important ? 'tomato' : 'black',
@@ -15,7 +23,7 @@ export default class TodoListItem extends Component {
     };
 
     return (
-      <span className="todo-list-item">
+      <span className={classNames}>
         <span className="todo-list-item-label" style={style} onClick={this.onLabelClick}>
           {label}
         </span>
