404d9e5f68cf005c16f394742fe0eaeedd80adaa
boka <beni.r@panorays.com>
2022-05-06 23:00:25 +0300

    fix: added item form

--- src/components/item-add-from/item-add-from.js
+++ src/components/item-add-from/item-add-from.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import './item-add-from.css';
+
 export default class ItemAddFrom extends Component {
+  state = { label: '' };
+
+  onLabelChange = e => {
+    this.setState({ label: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.onItemAdded(this.state.label);
+  };
+
   render() {
     return (
-      <div className="item-add-form">
-        <button className="btn btn-outline-secondary" onClick={() => this.props.onItemAdded('Hello world!')}>
-          Add Item
-        </button>
-      </div>
+      <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+        <input type="text" className="form-control" onChange={this.onLabelChange} placeholder="What needs to be done"></input>
+        <button className="btn btn-outline-secondary"> Add Item </button>
+      </form>
     );
   }
 }
