85d1542a82b4e34b218d16c8f61fa5f51b482c59
boka <beni.r@panorays.com>
2022-05-06 23:27:36 +0300

    fix: wip

--- src/components/item-add-from/item-add-from.js
+++ src/components/item-add-from/item-add-from.js
@@ -11,12 +11,19 @@ export default class ItemAddFrom extends Component {
   onSubmit = e => {
     e.preventDefault();
     this.props.onItemAdded(this.state.label);
+    this.setState({ label: '' });
   };
 
   render() {
     return (
       <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
-        <input type="text" className="form-control" onChange={this.onLabelChange} placeholder="What needs to be done"></input>
+        <input
+          type="text"
+          className="form-control"
+          onChange={this.onLabelChange}
+          placeholder="What needs to be done"
+          value={this.state.label}
+        ></input>
         <button className="btn btn-outline-secondary"> Add Item </button>
       </form>
     );
