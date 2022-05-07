9f29fb411eb5a37a0579c5b63a03cd9d1e1a6608
boka <beni.r@panorays.com>
2022-05-07 23:10:42 +0300

    fix: active filter buttons

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,9 +8,11 @@ import './app.css';
 
 export default class App extends Component {
   maxId = 100;
+
   state = {
-    todoData: [this.createTodoItem('Drink'), this.createTodoItem('Eat'), this.createTodoItem('Sleep')],
+    todoData: [this.createTodoItem('drink'), this.createTodoItem('eat'), this.createTodoItem('sleep')],
     term: '',
+    filter: 'all',
   };
 
   createTodoItem(label) {
@@ -26,7 +28,7 @@ export default class App extends Component {
   };
 
   addItem = text => {
-    const newItem = this.createTodoItem(text);
+    const newItem = this.createTodoItem(text.toLowerCase());
 
     this.setState(({ todoData }) => {
       const newArray = [...todoData, newItem];
@@ -34,12 +36,12 @@ export default class App extends Component {
     });
   };
 
-  toggleProperty(arr, id, propName) {
+  toggleProperty = (arr, id, propName) => {
     const idx = arr.findIndex(el => el.id === id);
     const oldItem = arr[idx];
     const newItem = { ...oldItem, [propName]: !oldItem[propName] };
     return [...arr.slice(0, idx), newItem, ...arr.slice(idx + 1)];
-  }
+  };
 
   onToggleImportant = id => {
     this.setState(({ todoData }) => {
@@ -53,7 +55,20 @@ export default class App extends Component {
     });
   };
 
-  search(items, term) {
+  filter = (items, filter) => {
+    switch (filter) {
+      case 'all':
+        return items;
+      case 'active':
+        return items.filter(item => !item.done);
+      case 'done':
+        return items.filter(item => item.done);
+      default:
+        return items;
+    }
+  };
+
+  search = (items, term) => {
     if (term.length === 0) {
       return items;
     }
@@ -61,15 +76,19 @@ export default class App extends Component {
     return items.filter(item => {
       return item.label.toLowerCase().indexOf(term.toLowerCase()) > -1;
     });
-  }
+  };
+
+  onFilterChange = filter => {
+    this.setState({ filter });
+  };
 
   onSearchChange = term => {
     this.setState({ term });
   };
 
   render() {
-    const { todoData, term } = this.state;
-    const visibleItems = this.search(todoData, term);
+    const { todoData, term, filter } = this.state;
+    const visibleItems = this.filter(this.search(todoData, term), filter);
     const doneCount = todoData.filter(el => el.done).length;
     const todoCount = todoData.length - doneCount;
 
@@ -78,7 +97,7 @@ export default class App extends Component {
         <AppHeader toDo={todoCount} done={doneCount} />
         <div className="top-panel d-flex">
           <SearchPanel onSearchChange={this.onSearchChange} />
-          <ItemStatusFilter />
+          <ItemStatusFilter filter={filter} onFilterChange={this.onFilterChange} />
         </div>
         <TodoList
           todos={visibleItems}
